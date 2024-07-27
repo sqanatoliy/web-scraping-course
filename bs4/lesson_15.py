70485732bcb855da1bdb8d563feef8e5f49e7e41
a.olesh <a.olesh@e-docs.ua>
2024-07-27 23:04:00 +0300

    bs4 - done

---
+++ bs4/lesson_15.py
@@ -0,0 +1,55 @@
+"""
+lessons 9-13 of the course "Web Scraping in Python Selenium, Scrapy + ChatGPT Prize 2024" on the site "Udemy"
+"""
+import time
+
+from bs4 import BeautifulSoup
+import requests
+
+#####################################################
+# Extracting the links of multiple movie transcripts
+#####################################################
+
+# How To Get The HTML
+root = 'https://subslikescript.com'  # this is the homepage of the website
+website = f'{root}/movies'  # concatenating the homepage with the movies section
+result = requests.get(website)
+content = result.text
+soup = BeautifulSoup(content, 'lxml')
+# print(soup.prettify())  # prints the HTML of the website
+
+# Locate the box that contains a list of movies
+box = soup.find('article', class_='main-article')
+
+# Store each link in "links" list (href doesn't consider root aka "homepage", so we have to concatenate it later)
+links = []
+for link in box.find_all('a', href=True):  # find_all returns a list
+    # print(link["href"])
+    links.append(link['href'])
+
+#################################################
+# Extracting the movie transcript
+#################################################
+
+# Loop through the "links" list and sending a request to each link
+# res = requests.get(f'{root}{links[0]}')
+# print(res.text)
+for link in links:
+    time.sleep(1)
+    result = requests.get(f'{root}{link}')
+    # print(result.text)
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    # Locate the box that contains title and transcript
+    box = soup.find('article', class_='main-article')
+    # Locate title and transcript
+    title = box.find('h1').get_text()
+    # print("Title 1: ", title)
+    title = ''.join(title.split('/'))
+    # print("Title 2: ", title)
+    transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+    # Exporting data in a text file with the "title" name
+    with open(f'{title}.txt', 'w') as file:
+        file.write(transcript)
