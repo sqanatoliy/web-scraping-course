1f7dcb226ffba91b5893f79d20ef18b68c7556fd
a.olesh <a.olesh@e-docs.ua>
2024-07-31 22:17:33 +0300

    savo to mongo done

---
+++ spider_tutorial/spider_tutorial/pipelines.py
@@ -0,0 +1,26 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+import logging
+import pymongo
+
+
+class MongodbPipeline:
+    collection_name = 'transcripts'
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient("mongodb+srv://sqanatoliy:sqanatoliy@cluster0.iauexdl.mongodb.net/?retryWrites=true&w=majority")
+        self.db = self.client['My_Database']
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
+        return item
